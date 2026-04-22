7e1a104e008158f4dd7a6d516a9cc77435097347
Mads-Laursen <58849@student.eadania.dk>
2026-04-22 14:07:42 +0200

    some_things

--- src/Drone_Nav.cpp
+++ src/Drone_Nav.cpp
@@ -1,18 +1,64 @@
 #include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <TinyGPS++.h>
 
-// put function declarations here:
-int myFunction(int, int);
+TinyGPSPlus gps;
+HardwareSerial gpsSerial(1);
+
+#define GPS_RX 3 
+
+void updateGPS()
+{
+  while (gpsSerial.available())
+  {
+    gps.encode(gpsSerial.read());
+  }
+}
+
+bool hasGoodGPSFix()
+{
+  if (!gps.location.isValid()) return false;
+  if (gps.satellites.value() < 5) return false;
+
+  // Optional: also require fresh data
+  if (gps.location.age() > 2000) return false;
+
+  return true;
+}
+
+void GetGPSData()
+{
+  updateGPS();
+
+  if (!hasGoodGPSFix())
+  {
+    Serial.print("Satellites: ");
+    Serial.println(gps.satellites.value());
+    Serial.print("Longitude: ");
+    Serial.println(gps.location.lng(), 7);
+    Serial.print("Latitude: "); 
+    Serial.println(gps.location.lat(), 7);
+
+  }
+}
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+ Serial.begin(115200);
+ gpsSerial.begin(9600, SERIAL_8N1, GPS_RX);
 
+  while (!hasGoodGPSFix())
+  {
+    updateGPS();
+
+    Serial.print("Satellites: ");
+    Serial.println(gps.satellites.value());
+  }
+}
+    
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  
+  updateGPS();
+  GetGPSData();
 }
